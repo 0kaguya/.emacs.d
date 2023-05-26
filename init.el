45370d3cb55a1f7891a9e9d99ebfd90173bcac6f
0kaguya <my@kaguya.org>
2023-05-26 21:34:43 +0800

    add typescript

--- init.el
+++ init.el
@@ -126,3 +126,18 @@
   (pyim-basedict-enable))
 
 
+;; typescript support
+(use-package tide
+  :ensure t
+  :config
+  (use-package typescript-mode
+    :ensure t)
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
